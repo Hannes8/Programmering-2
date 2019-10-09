c1d20613e25f10b5209d8b7dd98211ca17cad851
Hannes8 <hannes.kaulio@gmail.com>
2019-10-09 19:24:34 +0200

    First commit
    Började göra layouten

---
+++ src/Calculator/CalculatorLayout.java
@@ -0,0 +1,108 @@
+package Calculator;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.shape.Rectangle;
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+import javafx.scene.control.Button;
+import java.time.Clock;
+import java.util.ArrayList;
+
+public class CalculatorLayout extends Application {
+    ArrayList<Button> numPad = new ArrayList<Button>();
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        buttonCreator();
+        BorderPane calculatorLayout = new BorderPane();
+        calculatorLayout.setPrefWidth(1000);
+        calculatorLayout.setPrefHeight(800);
+
+        calculatorLayout.setTop(textInput());
+        calculatorLayout.setCenter(borderpaneCenter());
+
+        Scene calculatorScene = new Scene(calculatorLayout);
+
+        primaryStage.setScene(calculatorScene);
+        primaryStage.show();
+
+    }
+
+    public HBox textInput() {
+        HBox hbox = new HBox();
+        TextField input = new TextField("hello");
+        input.setPrefWidth(800.0);
+        input.setPrefHeight(200.0);
+        String hej = input.textProperty().get();
+        System.out.println(hej);
+
+
+        hbox.getChildren().addAll(input);
+        return hbox;
+
+
+    }
+
+    /**
+     * Skapar numpaden
+     * @return
+     */
+    public StackPane borderpaneCenter(){
+        int row = 1;
+        int columb= 1;
+        StackPane center = new StackPane();
+        GridPane buttons = new GridPane();
+        buttons.setHgap(10);
+        buttons.setVgap(10);
+        for (int i = 0; i < numPad.size(); i++) {
+
+            // var tredje iteration byter den rad o
+            if (i % 3 ==0) {
+                row++;
+                columb = 1;
+            }
+
+            buttons.add(numPad.get(i),columb,row);
+            columb ++;
+        }
+
+        center.getChildren().add(buttons);
+
+        return center;
+    }
+
+    public void buttonCreator() {
+
+        char[] numPadChar = {'7', '8', '9','4','5','6','1','2','3','0','.','='};
+        int count = 0;
+
+        for (char t : numPadChar){
+
+            char temporaryChar = numPadChar[count];
+            System.out.println(temporaryChar);
+            String temporaryString = String.valueOf(temporaryChar);
+            Button temp = new Button(temporaryString);
+            temp.setOnAction(event -> {System.out.println(temporaryString);});
+            temp.setPrefWidth(100);
+            temp.setPrefHeight(100);
+            numPad.add(temp);
+            count++;
+        }
+
+
+    }
+}
